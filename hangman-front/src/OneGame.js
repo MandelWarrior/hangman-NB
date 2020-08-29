3ebad7e67d646103a42b3d969dba2a55be47fa2c
Alan Sartorio <asartorio@itba.edu.ar>
2020-08-29 18:02:03 -0300

    Added game logic class.

---
+++ hangman-front/src/OneGame.js
@@ -0,0 +1,62 @@
+const TRIES = 6;
+
+function stringTranslate(str, setFrom, setTo) {
+
+    var newStr = '';
+    for (var i = 0; i < str.length; i++) {
+        newStr += setFrom.includes(str[i]) ? setTo[setFrom.indexOf(str[i])] : str[i];
+    }
+    return newStr;
+}
+
+export class OneGame {
+    constructor(word) {
+        this.lives = TRIES;
+
+        this.word = word.toUpperCase();
+        this.rawWord = stringTranslate(word, 'ÁÉÍÓÚÄËÏÖÜ', 'AEIOUAEIOU');
+
+        this.usedLetters = [];
+        this.hiddenLetters = [];
+        for (var i = 0; i < word.length; i++) {
+            this.hiddenLetters.push(true);
+        }
+    }
+
+    revealLetter(letter) {
+        let indices = [];
+
+        if (!this.usedLetters.includes(letter)) {
+            this.usedLetters.push(letter);
+
+            for (var i = 0; i < this.word.length; i++) {
+                if (this.rawWord[i] === letter) {
+                    indices.push(i);
+                    this.hiddenLetters[i] = false;
+                }
+            }
+
+            if (indices.length === 0) {
+                this.lives--;
+            }
+        }
+
+        return indices;
+    }
+
+    didLose() {
+        return this.lives === 0;
+    }
+
+    didWin() {
+        return this.hiddenLetters.every(h => !h);
+    }
+
+    getLetters() {
+        return this.word.map((l, i) => this.hiddenLetters[i] ? '_' : l);
+    }
+
+}
+
+
+export default OneGame;
