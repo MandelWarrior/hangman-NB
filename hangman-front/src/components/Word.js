9a16f93a1c6f5c7c3135bf6b83b352112e574196
Alan Sartorio <asartorio@itba.edu.ar>
2020-08-29 18:04:09 -0300

    Added the ability to hide and show individual characters in the Word component.

--- hangman-front/src/components/Word.js
+++ hangman-front/src/components/Word.js
@@ -4,15 +4,39 @@ import './Word.css';
 
 export class Word extends Component {
 
+    constructor(props) {
+        super(props);
+        this.letters = [];
+    }
+
+    setLetterRef(i, instance) {
+        if (instance === null)
+            this.letters.splice(i);
+        this.letters[i] = instance;
+    }
+    
     render() {
+        this.letters.length = 0;
         var letters = [];
         [...this.props.word].forEach((char, index) => {
-            letters.push(<Letter key={index} letter={char}/>);
+            letters.push(<Letter ref={this.setLetterRef.bind(this, index)} key={index} letter={char}/>);
         });
 
         return (
-            <div className='word'>{letters}</div>
+            <div className='word justify-content-center m-3'>{letters}</div>
         )
+    }
+
+    setLetterHidden(i, state) {
+        this.letters[i].setHidden(state);
+    }
+
+    showAllLetters() {
+        this.letters.forEach(l => l.setHidden(false));
+    }
+
+    hideAllLetters() {
+        this.letters.forEach(l => l.setHidden(true));
     }
 }
 
