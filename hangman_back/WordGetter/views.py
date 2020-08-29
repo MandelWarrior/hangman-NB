1ef265fd21cfc0675463eb6446d3ef7f61c6e39b
Alan Sartorio <asartorio@itba.edu.ar>
2020-08-29 17:54:35 -0300

    Another word list was added and the word-getter response is now encoded in latin-1.

--- hangman_back/WordGetter/views.py
+++ hangman_back/WordGetter/views.py
@@ -8,10 +8,10 @@ import os
 module_dir = os.path.dirname(__file__)
 
 
-wordGetter = WordGetter(os.path.join(module_dir, 'words.txt'))
-#wordGetter.start()
+#wordGetter = WordGetter(os.path.join(module_dir, 'words_2.txt', encoding='latin-1'))
+wordGetter = WordGetter(os.path.join(module_dir, 'words.txt'), encoding='utf-8')
 
 # Create your views here.
 def getWord(request):
     if request.method == 'GET':
-        return HttpResponse(wordGetter.getWord(), content_type="text/plain")
+        return HttpResponse(wordGetter.getWord(), content_type="text/plain", charset='latin-1')
