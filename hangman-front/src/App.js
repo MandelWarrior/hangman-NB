41ed141870e80dad2634e19c5cca4b222d36c529
Alan Sartorio <asartorio@itba.edu.ar>
2020-08-29 18:10:08 -0300

    Main App component changes:
    Added more state management to work with the game logic
    
    The word response is now decoded in latin-1

--- hangman-front/src/App.js
+++ hangman-front/src/App.js
@@ -1,39 +1,80 @@
 import React, { Component } from "react";
 
 import Word from "./components/Word";
+import Hangman from "./components/Hangman";
 
 import "./App.css";
+import GameOver from "./components/GameOver";
+import Win from "./components/Win";
+import Title from "./components/Title";
+
+import { Button, Row, Col } from "react-bootstrap";
+import Keyboard from "./components/Keyboard";
+import OneGame from "./OneGame";
 
 export class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { word: "" };
+    this.state = { playing: false, game: null };
   }
 
   componentDidMount() {
+
+    this.newGame();
+
+  }
+
+  newGame() {
     this.getWord().then((word) => {
-      console.log(word);
-      this.setState({ word });
+      if (word !== null) {
+        let game = new OneGame(word);
+        this.setState({ playing: true, game });
+        this.keyboard.enableAll();
+        this.word.hideAllLetters();
+      }
     });
   }
 
   async getWord() {
-    var res = await fetch("/word-getter", {
-      method: "GET",
-      headers: {
-        "Content-Type": "text/plain",
-      },
-    })
-      .then((response) => {
-        if (!response.ok)
-          throw new Error("Not OK response");
-        return response.text();
-      })
-      .catch(() => {
-        console.log('error!!!');
-        return null;
+    try {
+      let response = await fetch("/word-getter", {
+        method: "GET",
+        headers: {
+          "Content-Type": "text/plain",
+        },
       });
-    return res;
+
+      if (!response.ok)
+        throw new Error("Not OK response");
+
+      let buffer = await response.arrayBuffer();
+
+      let decoder = new TextDecoder("iso-8859-1");
+      let text = decoder.decode(buffer);
+      return text;
+
+    } catch (error) {
+      console.log('error!!!');
+      return null;
+    }
+  }
+
+  keyboardPressed(k) {
+    console.log(k);
+    this.keyboard.disableKey(k);
+    let indices = this.state.game.revealLetter(k);
+    indices.forEach((i) => {
+      this.word.setLetterHidden(i, false);
+    });
+    let lose = this.state.game.didLose();
+    let win = this.state.game.didWin();
+    if (lose || win) {
+      this.word.showAllLetters();
+      this.setState({ playing: false });
+      this.setState({ win, lose });
+    } else {
+      this.setState({});
+    }
   }
 
   render() {
@@ -41,11 +82,30 @@ export class App extends Component {
       <div className="App">
         <header className="App-header">
           {
-            this.state.word === null ? <h1>Error!!</h1> : this.state.word !== "" ? (
-              <Word word={this.state.word} />
-            ) : (
-                <h1>Cargando...</h1>
-              )
+            this.state.playing
+              ? <Title />
+              : this.state.win
+                ? <Win />
+                : <GameOver />
+
+          }
+          {
+            this.state.game === null ?
+              <h1>Loading...</h1>
+              :
+              <Row >
+                <Hangman lives={this.state.game.lives} />
+                <Col className="align-self-center">
+                  <div className="align-self-center">
+                    <Word ref={(instance) => { this.word = instance; }} word={this.state.game.word} />
+                  </div>
+                  {
+                    this.state.playing
+                      ? <Keyboard ref={(instance) => this.keyboard = instance} onKeyPressed={this.keyboardPressed.bind(this)} />
+                      : <Button className="m-3" onClick={() => { this.newGame(); }}>Restart</Button>
+                  }
+                </Col>
+              </Row>
           }
         </header>
       </div>
