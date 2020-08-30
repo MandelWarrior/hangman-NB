f534a4de808c74816a3dc15b36fa2c384392fffc
MandelWarrior <alan42ga@hotmail.com>
2020-08-29 22:46:45 -0300

    Updates

---
+++ precache-manifest.8d2a466010fabbefc7a0bb192401fee4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff390128fd428c5d3ea7c694db7bd8ef",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "64a914fb90552b47b1bb",
+    "url": "/hangman/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8463f79944573eeea771",
+    "url": "/hangman/static/css/main.376945c5.chunk.css"
+  },
+  {
+    "revision": "64a914fb90552b47b1bb",
+    "url": "/hangman/static/js/2.9838c0d2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/hangman/static/js/2.9838c0d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8463f79944573eeea771",
+    "url": "/hangman/static/js/main.ad041531.chunk.js"
+  },
+  {
+    "revision": "7be8f85010270d4e8459",
+    "url": "/hangman/static/js/runtime-main.1e5ba2ce.js"
+  },
+  {
+    "revision": "0a40eb6f99c8d0cccc07d1fa924eb746",
+    "url": "/hangman/static/media/mononoki-Regular.0a40eb6f.ttf"
+  }
+]);
