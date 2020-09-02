2ee0f32e36fbced7c31a49ea621036a7235b1907
MandelWarrior <alan42ga@hotmail.com>
2020-09-02 16:08:10 -0300

    Now using react router.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 ReactDOM.render(
+
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={'/hangman-NB'}>
+      <Route path="/" component={App} />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
